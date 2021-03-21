cd30493bd09b9694eb7e457034e73e3bc60c5c92
Cleiton Lima <cleiton.inside@gmail.com>
2021-03-21 16:20:04 -0300

    First commit: project structure created

---
+++ src/test/java/com/cleitonlima/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleitonlima.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
