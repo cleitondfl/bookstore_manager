f197fbadb1c0f81b61404e262dee809607262413
Cleiton Lima <cleiton.inside@gmail.com>
2021-03-21 18:15:43 -0300

    Developed project entities

---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,45 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "publisher_name", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public void author() {
+        author.getName();
+    }
+}
--- src/main/java/entity/BookstoremanagerApplication.java
+++ src/main/java/entity/BookstoremanagerApplication.java
@@ -1,4 +1,4 @@
-package com.cleitonlima.bookstoremanager;
+package entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
